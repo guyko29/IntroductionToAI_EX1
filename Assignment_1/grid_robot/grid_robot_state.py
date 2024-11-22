79cdfc0a291902feb1525ad00413121892bbc378
guyko29 <168747799+guyko29@users.noreply.github.com>
2024-11-22 13:58:32 +0200

    Add files via upload

---
+++ Assignment_1/grid_robot/grid_robot_state.py
@@ -0,0 +1,27 @@
+class grid_robot_state:
+    # you can add global params
+
+    def __init__(self, robot_location, map=None, lamp_height=-1, lamp_location=(-1, -1)):
+        # you can use the init function for several purposes
+        pass
+
+    @staticmethod
+    def is_goal_state(_grid_robot_state):
+        pass
+
+    def get_neighbors(self):
+        pass
+
+        # you can change the body of the function if you want
+        # def __hash__(self):
+
+        # you can change the body of the function if you want
+        # def __eq__(self, other):
+        # you can change the body of the function if you want
+
+    def get_state_str(self):
+        return self.location
+
+
+
+    #you can add helper functions
---
+++ Assignment_1/grid_robot/heuristics.py
@@ -0,0 +1,10 @@
+from grid_robot_state import grid_robot_state
+
+
+
+
+def base_heuristic(_grid_robot_state):
+    return 0
+
+def advanced_heuristic(_grid_robot_state):
+    return 0
