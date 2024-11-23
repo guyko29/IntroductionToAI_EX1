04dd929ea5bd00799c0675908a06e76eb1ec88f8
yuval livshtits <yuvall0912@gmail.com>
2024-11-23 18:00:45 +0200

    finished grid_robot_state.py functions

--- Assignment_1/grid_robot/grid_robot_state.py
+++ Assignment_1/grid_robot/grid_robot_state.py
@@ -3,14 +3,43 @@ class grid_robot_state:
 
     def __init__(self, robot_location, map=None, lamp_height=-1, lamp_location=(-1, -1)):
         # you can use the init function for several purposes
-        pass
+        self.robot_location = tuple(robot_location)
+        self.map = map
+        self.lamp_height = lamp_height
+        self.lamp_location = lamp_location
 
     @staticmethod
     def is_goal_state(_grid_robot_state):
-        pass
+        if _grid_robot_state.lamp_location == _grid_robot_state.robot_location:
+            return True
+        return False
 
     def get_neighbors(self):
-        pass
+        neighbors = []
+        down = (1,0)
+        up = (-1,0)
+        left = (0,-1)
+        right = (0,1)
+        directions = [down, up, left, right]
+        current_row , current_col = self.robot_location
+
+        #Generate the new neighbors if possible
+        for row_delta, col_delta in directions:
+            new_row = current_row + row_delta
+            new_col = current_col + col_delta
+
+            if 0 <= new_row < len(self.map) and 0 <= new_col < len(self.map[0]):
+                if self.map[new_row][new_col] != -1:
+                    new_state = grid_robot_state(
+                        robot_location=(new_row, new_col),
+                        map=self.map,
+                        lamp_height=self.lamp_height,
+                        lamp_location=self.lamp_location
+                    )
+                    cost = 1
+                    neighbors.append((new_state, cost))
+
+        return neighbors
 
         # you can change the body of the function if you want
         # def __hash__(self):
@@ -20,8 +49,9 @@ class grid_robot_state:
         # you can change the body of the function if you want
 
     def get_state_str(self):
-        return self.location
+        pass
 
 
-
+    def get_lamp_location(self):
+        return self.lamp_location
     #you can add helper functions
--- Assignment_1/grid_robot/main.py
+++ Assignment_1/grid_robot/main.py
@@ -1,16 +1,20 @@
 
 from heuristics import *
-from grid_robot_solved.search import *
+#from grid_robot_solved.search import *
 
 if __name__ == '__main__':
     map = [
         [0, 0, 0, 0],
         [1, 4, 2, -1],
-        [0, -1, 0, -1]
+        [0, 0, 0, -1]
     ]
-    robot_start_location = (0, 0)
+    robot_start_location = (1, 1)
     lamp_h = 6
     lamp_location = (2, 2)
 
-    start_state = grid_robot_state(map, robot_start_location, lamp_h, lamp_location)
-    search_result = search(start_state, base_heuristic)
+    start_state = grid_robot_state(robot_start_location,map, lamp_h, lamp_location)
+    neighbors = start_state.get_neighbors()
+    for neighbor, cost in neighbors:
+        print(f"Neighbor: {neighbor.robot_location}, Cost: {cost}")
+
+    #search_result = search(start_state, base_heuristic)
