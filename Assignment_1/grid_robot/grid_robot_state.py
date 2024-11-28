90fdf83c6b9687bd883f974fed0f6b71974a2af3
guyko29 <168747799+guyko29@users.noreply.github.com>
2024-11-28 14:57:10 +0200

    Update grid_robot_state.py

--- Assignment_1/grid_robot/grid_robot_state.py
+++ Assignment_1/grid_robot/grid_robot_state.py
@@ -1,57 +1,100 @@
 class grid_robot_state:
-    # you can add global params
-
-    def __init__(self, robot_location, map=None, lamp_height=-1, lamp_location=(-1, -1)):
-        # you can use the init function for several purposes
+    def __init__(self, robot_location, map=None, lamp_height=-1, lamp_location=(-1, -1), stairs_height=0, carrying=False):
         self.robot_location = tuple(robot_location)
         self.map = map
         self.lamp_height = lamp_height
-        self.lamp_location = lamp_location
+        self.lamp_location = tuple(lamp_location)
+        self.stairs_height = stairs_height
+        self.carrying = carrying  # Indicates whether the robot is carrying stairs
 
     @staticmethod
-    def is_goal_state(_grid_robot_state):
-        if _grid_robot_state.lamp_location == _grid_robot_state.robot_location:
-            return True
-        return False
+    def is_goal_state(state):
+        # The robot is at the lamp location, and the stairs at the lamp location equal the lamp height.
+        if state.robot_location != state.lamp_location:
+            return False
+        row, col = state.lamp_location
+        return state.map[row][col] == state.lamp_height
 
     def get_neighbors(self):
         neighbors = []
-        down = (1,0)
-        up = (-1,0)
-        left = (0,-1)
-        right = (0,1)
-        directions = [down, up, left, right]
-        current_row , current_col = self.robot_location
+        directions = [(1, 0), (-1, 0), (0, -1), (0, 1)]
+        current_row, current_col = self.robot_location
 
-        #Generate the new neighbors if possible
-        for row_delta, col_delta in directions:
-            new_row = current_row + row_delta
-            new_col = current_col + col_delta
+        # Move in four directions
+        for row_step, col_step in directions:
+            new_row = current_row + row_step
+            new_col = current_col + col_step
+            if 0 <= new_row < len(self.map) and 0 <= new_col < len(self.map[0]) and self.map[new_row][new_col] != -1:
+                cost = 1 + (self.stairs_height if self.carrying else 0)
+                neighbor = grid_robot_state(
+                    robot_location=(new_row, new_col),
+                    map=self.map,
+                    lamp_height=self.lamp_height,
+                    lamp_location=self.lamp_location,
+                    stairs_height=self.stairs_height,
+                    carrying=self.carrying
+                )
+                neighbors.append((neighbor, cost))
 
-            if 0 <= new_row < len(self.map) and 0 <= new_col < len(self.map[0]):
-                if self.map[new_row][new_col] != -1:
-                    new_state = grid_robot_state(
-                        robot_location=(new_row, new_col),
-                        map=self.map,
-                        lamp_height=self.lamp_height,
-                        lamp_location=self.lamp_location
-                    )
-                    cost = 1
-                    neighbors.append((new_state, cost))
+        # Pick up stairs
+        if not self.carrying and self.map[current_row][current_col] > 0:
+            neighbor = grid_robot_state(
+                robot_location=self.robot_location,
+                map=self.map,
+                lamp_height=self.lamp_height,
+                lamp_location=self.lamp_location,
+                stairs_height=self.map[current_row][current_col],
+                carrying=True
+            )
+            cost = 1  # Picking up stairs
+            neighbors.append((neighbor, cost))
+
+        # Place stairs
+        if self.carrying and self.map[current_row][current_col] >= 0:
+            new_map = [row[:] for row in self.map]
+            new_map[current_row][current_col] += self.stairs_height
+            neighbor = grid_robot_state(
+                robot_location=self.robot_location,
+                map=new_map,
+                lamp_height=self.lamp_height,
+                lamp_location=self.lamp_location,
+                stairs_height=0,
+                carrying=False
+            )
+            cost = 1  # Placing stairs
+            neighbors.append((neighbor, cost))
+
+        # Combine stairs
+        if self.carrying and self.map[current_row][current_col] > 0:
+            new_stairs_height = self.stairs_height + self.map[current_row][current_col]
+            if new_stairs_height <= self.lamp_height:
+                new_map = [row[:] for row in self.map]
+                new_map[current_row][current_col] = 0  # Clear stairs after combining
+                neighbor = grid_robot_state(
+                    robot_location=self.robot_location,
+                    map=new_map,
+                    lamp_height=self.lamp_height,
+                    lamp_location=self.lamp_location,
+                    stairs_height=new_stairs_height,
+                    carrying=True
+                )
+                cost = 1  # Combining stairs
+                neighbors.append((neighbor, cost))
 
         return neighbors
 
-        # you can change the body of the function if you want
-        # def __hash__(self):
-
-        # you can change the body of the function if you want
-        # def __eq__(self, other):
-        # you can change the body of the function if you want
-
     def get_state_str(self):
-        pass
+        carrying_str = "carrying" if self.carrying else "not_carrying"
+        return f"{self.robot_location}_{self.lamp_location}_{carrying_str}"
 
+    def __hash__(self):
+        return hash((self.robot_location, self.lamp_location, self.carrying))
 
-    def get_lamp_location(self):
-        return self.lamp_location
-    #you can add helper functions
+    def __eq__(self, other):
+        if not isinstance(other, grid_robot_state):
+            return False
+        return (
+            self.robot_location == other.robot_location
+            and self.lamp_location == other.lamp_location
+            and self.carrying == other.carrying
+        )
