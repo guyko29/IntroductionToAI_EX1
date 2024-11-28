0efc4a9d00abccf7aa30322e9e2bb2496ba52553
guyko29 <168747799+guyko29@users.noreply.github.com>
2024-11-28 14:57:30 +0200

    Update heuristics.py

--- Assignment_1/grid_robot/heuristics.py
+++ Assignment_1/grid_robot/heuristics.py
@@ -1,10 +1,16 @@
 from grid_robot_state import grid_robot_state
 
-
-
-
 def base_heuristic(_grid_robot_state):
-    return 0
+    robot_row, robot_col = _grid_robot_state.robot_location
+    lamp_row, lamp_col = _grid_robot_state.lamp_location
+    return abs(robot_row - lamp_row) + abs(robot_col - lamp_col)
+
 
 def advanced_heuristic(_grid_robot_state):
-    return 0
+    robot_row, robot_col = _grid_robot_state.robot_location
+    lamp_row, lamp_col = _grid_robot_state.lamp_location
+    manhattan_distance = abs(robot_row - lamp_row) + abs(robot_col - lamp_col)
+    if _grid_robot_state.carrying:
+        return manhattan_distance + _grid_robot_state.stairs_height
+    else:
+        return manhattan_distance + (_grid_robot_state.lamp_height - _grid_robot_state.stairs_height)
